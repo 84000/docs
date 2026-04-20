85c6dc4d86e1fe64beb2c6ec8608b8a674ffc385
Grant Damron <grant@ilusiv.io>
2026-04-20 13:06:56 -0700

    Add docs prefix (#3)
    Restore the docs prefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 //@ts-check
 
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import nextra from 'nextra';
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import nextra from "nextra";
 
 const rootDir = dirname(fileURLToPath(import.meta.url));
 
@@ -10,12 +10,16 @@ const rootDir = dirname(fileURLToPath(import.meta.url));
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
+  assetPrefix: process.env.NODE_ENV === "production" ? "/docs" : undefined,
   outputFileTracingRoot: rootDir,
   turbopack: {
     root: rootDir,
   },
 };
 
-const withNextra = nextra({});
+const withNextra = nextra({
+  contentDirBasePath:
+    process.env.NODE_ENV === "production" ? "/docs" : undefined,
+});
 
 export default withNextra(nextConfig);
